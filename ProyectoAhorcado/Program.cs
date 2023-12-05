14fd731811f4ee2ac9f85a3f459fdcdabbbddd49
Alexis0003 <A342110014@unidem.edu.mx>
2023-12-05 14:53:00 -0600

    Alexis - Interfaz de Usuario
    Metodos MostrarEstadoJuego,ObtenerLetra y ActualizarPalabraMostrada////Cambio de la parte de Alan por error al ejecutar una prueba(metodos mal acomodados)

--- ProyectoAhorcado/Program.cs
+++ ProyectoAhorcado/Program.cs
@@ -49,68 +49,93 @@ class Program
             Console.WriteLine("¡Oh no! Has agotado tus intentos. La palabra era: " + palabraSecreta);
             DibujarAhorcado(6);
         }
-
-        Console.ReadLine();
-        //Alan Omar Rendon Martinez(Estructura)
-        static void DibujarAhorcado(int errores)
+    }
+    //Alexis Manuel Espinoza Martinez - Interfaz de Usuario
+    //Este método muestra el estado actual del juego en la consola
+    static void MostrarEstadoJuego(char[] palabraMostrada, int intentosRestantes)
+    {
+        Console.WriteLine("Palabra: " + string.Join(" ", palabraMostrada));//Array que muestra al jugador las letras adivinadas reveladas y las que no estan ocultas
+        Console.WriteLine("Intentos restantes: " + intentosRestantes);//Numero que muestra la cantidad de vidas/intentos que le quedan al jugador
+    }
+    //Metodo que solicita al jugador que ingrese una letra
+    static char ObtenerLetra()
+    {
+        Console.Write("Ingresa una letra: ");
+        return Console.ReadLine()[0];// Devuelve el primer caracter de entrada del usuario
+    }
+    // Este método actualiza el array palabraMostrada con las letras adivinadas
+    static bool ActualizarPalabraMostrada(char letra, string palabraSecreta, char[] palabraMostrada)//letra adivinada por el jugador, la palabra que el jugador intenta adivinar, estado actual de la palabra
+    {
+        //El ciclo pasa sobre cada letra de la palabra a adivinar (palabraSecreta) y compara si coincide con la letra ingresada, de ser asi actualiza la posicion en palabraMostrada con la letra adivinada y establece letraAdivinada a true
+        bool letraAdivinada = false;
+        for (int i = 0; i < palabraSecreta.Length; i++)
         {
-            switch (errores)
+            if (char.ToUpper(palabraSecreta[i]) == char.ToUpper(letra))
             {
-                //El codigo funciona para actualizar el monito cada vez que se equivoque al ingrsar una letra incorrecta usando un switch para agregar
-                // y actualizar el disño del monito, una vez completado el monito se imprimira que el usuario ha perdido  
-                case 1:
-                    Console.WriteLine("  +---+");
-                    Console.WriteLine("  |   |");
-                    Console.WriteLine("      |");
-                    Console.WriteLine("      |");
-                    Console.WriteLine("      |");
-                    Console.WriteLine("      |");
-                    break;
-                case 2:
-                    Console.WriteLine("  +---+");
-                    Console.WriteLine("  |   |");
-                    Console.WriteLine("  O   |");
-                    Console.WriteLine("      |");
-                    Console.WriteLine("      |");
-                    Console.WriteLine("      |");
-                    break;
-                case 3:
-                    Console.WriteLine("  +---+");
-                    Console.WriteLine("  |   |");
-                    Console.WriteLine("  O   |");
-                    Console.WriteLine("  |   |");
-                    Console.WriteLine("      |");
-                    Console.WriteLine("      |");
-                    break;
-                case 4:
-                    Console.WriteLine("  +---+");
-                    Console.WriteLine("  |   |");
-                    Console.WriteLine("  O   |");
-                    Console.WriteLine(" -|   |");
-                    Console.WriteLine("      |");
-                    Console.WriteLine("      |");
-                    break;
-                case 5:
-                    Console.WriteLine("  +---+");
-                    Console.WriteLine("  |   |");
-                    Console.WriteLine("  O   |");
-                    Console.WriteLine(" -|-  |");
-                    Console.WriteLine("      |");
-                    Console.WriteLine("      |");
-                    break;
-                case 6:
-                    Console.WriteLine("  +---+");
-                    Console.WriteLine("  |   |");
-                    Console.WriteLine("  O   |");
-                    Console.WriteLine(" -|-  |");
-                    Console.WriteLine(" -+-  |");
-                    Console.WriteLine("      |");
-                    Console.WriteLine("GAME OVER");
-                    break;
-                default:
-                    break;
+                palabraMostrada[i] = palabraSecreta[i];
+                letraAdivinada = true;
             }
         }
-
+        return letraAdivinada;
+    }
+    //Alan Omar Rendon Martinez(Estructura)
+    static void DibujarAhorcado(int errores)
+    {
+        switch (errores)
+        {
+            //El codigo funciona para actualizar el monito cada vez que se equivoque al ingrsar una letra incorrecta usando un switch para agregar
+            // y actualizar el disño del monito, una vez completado el monito se imprimira que el usuario ha perdido  
+            case 1:
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                break;
+            case 2:
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                break;
+            case 3:
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                break;
+            case 4:
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine(" -|   |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                break;
+            case 5:
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine(" -|-  |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                break;
+            case 6:
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine(" -|-  |");
+                Console.WriteLine(" -+-  |");
+                Console.WriteLine("      |");
+                Console.WriteLine("GAME OVER");
+                break;
+            default:
+                break;
+        }
     }
 }
